8b2650a835186055bb6758f5d16300cb87070c68
JoelAngel <joelbaraka4325@gmail.com>
2021-12-12 12:35:11 +0300

    basic setup on the formik state management

--- src/components/accountBox/signupForm.jsx
+++ src/components/accountBox/signupForm.jsx
@@ -9,20 +9,61 @@ import {
 } from "./common";
 import { Marginer } from "../marginer";
 import { AccountContext } from "./accountContext";
+import { useFormik } from "formik";
 
 export function SignupForm(props) {
   const { switchToSignin } = useContext(AccountContext);
 
+  //this function currently takes the values that the current form has
+  const onSubmit = (values) => {
+    alert(JSON.stringify(values));
+  };
+
+  const formik = useFormik({
+    initialValues: {
+      fullName: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+    validateOnBlur: true,
+    onSubmit,
+  });
+
   return (
     <BoxContainer>
-      <FormContainer>
-        <Input type="text" placeholder="Full Name" />
-        <Input type="email" placeholder="Email" />
-        <Input type="password" placeholder="Password" />
-        <Input type="password" placeholder="Confirm Password" />
+      <FormContainer onSubmit={formik.handleSubmit}>
+        <Input
+          name="fullName"
+          placeholder="Full Name"
+          value={formik.values.fullName}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+        />
+        <Input
+          name="email"
+          // type="email"
+          placeholder="Email"
+          value={formik.values.email}
+          onChange={formik.handleChange}
+        />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Password"
+          value={formik.values.password}
+          onChange={formik.handleChange}
+        />
+        <Input
+          name="confirmPassword"
+          type="password"
+          placeholder="Confirm Password"
+          value={formik.values.confirmPassword}
+          onChange={formik.handleChange}
+        />
+        <SubmitButton type="submit">Signup</SubmitButton>
       </FormContainer>
       <Marginer direction="vertical" margin={10} />
-      <SubmitButton type="submit">Signup</SubmitButton>
       <Marginer direction="vertical" margin="1em" />
       <MutedLink href="#">
         Already have an account?
