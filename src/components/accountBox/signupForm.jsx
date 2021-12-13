4809e1afd50a22bf3d5697b6c2ff4630af4c7969
JoelAngel <joelbaraka4325@gmail.com>
2021-12-13 13:49:48 +0300

    added the form complex validation

--- src/components/accountBox/signupForm.jsx
+++ src/components/accountBox/signupForm.jsx
@@ -10,6 +10,30 @@ import {
 import { Marginer } from "../marginer";
 import { AccountContext } from "./accountContext";
 import { useFormik } from "formik";
+import * as yup from "yup";
+
+const PASSWORD_REGEX = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
+
+const validationSchema = yup.object({
+  fullName: yup
+    .string()
+    .min(3, "Please enter your real name")
+    .required("Full name is required"),
+  email: yup.string().email("Please enter a valid email address").required(),
+  password: yup
+    .string()
+    .matches(PASSWORD_REGEX, "Please enter a strong password")
+    .required(),
+  confirmPassword: yup
+    .string()
+    .required("Please confirm your password")
+    .when("password", {
+      is: (val) => (val && val.length > 0 ? true : false),
+      then: yup
+        .string()
+        .oneOf([yup.ref("password")], "Password does not match"),
+    }),
+});
 
 export function SignupForm(props) {
   const { switchToSignin } = useContext(AccountContext);
@@ -28,7 +52,9 @@ export function SignupForm(props) {
     },
     validateOnBlur: true,
     onSubmit,
+    validationSchema: validationSchema,
   });
+  // console.log("Error: ", formik.errors);
 
   return (
     <BoxContainer>
